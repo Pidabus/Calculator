b3607d4337341c39f65aaf8d1f8e5ebdbde79230
Pidabus <affiqfirdaus67@gmail.com>
2026-09-22 10:39:20 +0800

    made some progress, but some bugs

--- main/calc.js
+++ main/calc.js
@@ -20,15 +20,28 @@ function calculator() {
     let var2 = null;
     let operator = null;
 
-    function operate(operator, var1, var2) {
-        return operator(var1, var2);
-    }
-
     const inputs = document.querySelector(".inputs");
     const display = document.querySelector(".display");
     const clearButton = document.querySelector(".button");
     const inputs_operators = document.querySelector(".operators");
-    const equalButton = document.querySelector(".equals");
+    const equalButton = document.querySelector(".equal");
+
+    function operate(operator, var1, var2) {
+        switch (operator) {
+            case "+":
+                return add(var1, var2);
+                break;
+            case "-":
+                return subtract(var1, var2);
+                break;
+            case "x":
+                return multiply(var1, var2);
+                break;
+            case "/":
+                return divide(var1, var2);
+                break;
+        }
+    }
 
     inputs.addEventListener("click", (e) => {
         if(display.textContent != "") {
@@ -44,11 +57,23 @@ function calculator() {
     inputs_operators.addEventListener("click", (e) => {
         let operand = e.target.textContent;
 
-        var1 = display.textContent;
+        var1 = Number(display.textContent);
         operator = operand;
 
         display.textContent += " " + operand + " ";
     })
+
+    equalButton.addEventListener("click", (e) => {
+        let text = display.textContent;
+
+        var2 = text;
+
+        display.textContent = "";
+
+        let result = operate(operator, var1, var2);
+
+        display.textContent = result;
+        })
 
     clearButton.addEventListener("click", (e) => {
         display.textContent = "";
