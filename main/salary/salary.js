d2e9e5bab07d9da3737880f5a64ed4879d7ff020
Pidabus <affiqfirdaus67@gmail.com>
2026-09-22 13:47:12 +0800

    The salary calculator works, but many functions left to make. Right now, it can calculate your monthly & annual from your hourly

--- main/salary/salary.js
+++ main/salary/salary.js
@@ -6,13 +6,42 @@ function getAnnual(monthlySalary) {
     return monthlySalary * 12;
 }
 
-let hourly = input
-monthlySalary = getMonthly(hourly);
-annualSalary = getAnnual(monthlySalary);
+const hourlyView = document.querySelector(".hourly");
+const monthlyView = document.querySelector(".monthly");
+const annualView = document.querySelector(".annual");
+const startButton = document.querySelector("button");
 
-let output = 
-`Monthly salary is: \tRM${monthlySalary}
+let hourly = null;
+let monthlySalary = null;
+let annualSalary = null;
 
-Annual salary is: \tRM${annualSalary}`;
+function salaryCalculator() {
+    startButton.addEventListener("click", (e) => {
+        let getChoice = prompt("Input: \n1.Hourly salary \n2.Monthly salary \n3.Annual Salary");
 
-console.log(output);
+        let salaryChoice = null;
+        switch (getChoice) {
+            case "1":
+                salaryChoice = prompt("Your hourly salary in RM is:");
+                hourly = salaryChoice;
+                monthlySalary = getMonthly(hourly);
+                annualSalary = getAnnual(monthlySalary);
+                break;
+            case "2":
+                salaryChoice = prompt("Your monthly salary in RM is:");
+                break;
+            case "3":
+                salaryChoice = prompt("Your annual salary in RM is:");
+                break;
+            default:
+                alert("Not a valid choice!");
+        } 
+
+        hourlyView.textContent = `RM ${hourly}`;
+        monthlyView.textContent = `RM ${monthlySalary}`;
+        annualView.textContent = `RM ${annualSalary}`;
+    });
+
+}
+
+salaryCalculator();
