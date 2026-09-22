88365fe2007d635fd430c414bc2d2e297dafaad4
Pidabus <affiqfirdaus67@gmail.com>
2026-09-22 16:02:11 +0800

    MVP for the salary calculator works

--- main/salary/salary.js
+++ main/salary/salary.js
@@ -6,6 +6,19 @@ function getAnnual(monthlySalary) {
     return monthlySalary * 12;
 }
 
+function getHourly(salary, salaryType) {
+    if (salaryType === "monthly") {
+        let hourly = salary/176; // gets hourly form monthly
+
+        return Number(hourly.toFixed(2));
+    }
+    if (salaryType === "yearly") {
+        let hourly = salary/2112; //gets hourly from annual
+
+        return Number(hourly.toFixed(2));
+    }
+}
+
 const hourlyView = document.querySelector(".hourly");
 const monthlyView = document.querySelector(".monthly");
 const annualView = document.querySelector(".annual");
@@ -20,6 +33,7 @@ function salaryCalculator() {
         let getChoice = prompt("Input: \n1.Hourly salary \n2.Monthly salary \n3.Annual Salary");
 
         let salaryChoice = null;
+        let salaryType = null;
         switch (getChoice) {
             case "1":
                 salaryChoice = prompt("Your hourly salary in RM is:");
@@ -29,17 +43,25 @@ function salaryCalculator() {
                 break;
             case "2":
                 salaryChoice = prompt("Your monthly salary in RM is:");
+                salaryType = "monthly";
+                hourly = getHourly(salaryChoice, salaryType);
+                monthlySalary = salaryChoice;
+                annualSalary = getAnnual(monthlySalary);
                 break;
             case "3":
                 salaryChoice = prompt("Your annual salary in RM is:");
+                salaryType = "yearly";
+                hourly = getHourly(salaryChoice, salaryType);
+                monthlySalary = getMonthly(hourly);
+                annualSalary = salaryChoice;
                 break;
             default:
                 alert("Not a valid choice!");
-        } 
+        }
 
-        hourlyView.textContent = `RM ${hourly}`;
-        monthlyView.textContent = `RM ${monthlySalary}`;
-        annualView.textContent = `RM ${annualSalary}`;
+        hourlyView.textContent = `Hourly: RM ${hourly}`;
+        monthlyView.textContent = `Monthly: RM ${monthlySalary}`;
+        annualView.textContent = `Annual: RM ${annualSalary}`;
     });
 
 }
