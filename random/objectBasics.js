75885516f01e38af84d50bfb3094837822bd71e4
Pidabus <affiqfirdaus67@gmail.com>
2026-09-25 17:14:20 +0800

    .;'

--- random/objectBasics.js
+++ random/objectBasics.js
@@ -1,16 +1,22 @@
 let obj1 = {
     name: 'Affiq',
     surname: 'Firdaus',
+    age: 67, 
+    isAlive: true,
 }
 
-let obj2 = {}
+// let obj2 = {}
 
-obj2 = obj1;
+// obj2 = obj1;
 
-// obj2.name = 'Diddy';  ==> mutates both obj1 and obj2
-// obj2 = {  ==> Creates a new object
-//     name: 'Diddy',
+// // obj2.name = 'Diddy';  ==> mutates both obj1 and obj2
+// // obj2 = {  ==> Creates a new object
+// //     name: 'Diddy',
+// // }
+
+// console.log(obj1);
+// console.log(obj2);
+
+// for (let prop in obj1) {
+//     console.log(`Property: ${prop} - Value: ${obj1[prop]}`); // Remember to use [prop] for computed properties.
 // }
-
-console.log(obj1);
-console.log(obj2);
