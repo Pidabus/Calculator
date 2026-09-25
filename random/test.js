f6ddff223cd982c91ee436cdbb157b17165997e8
Pidabus <affiqfirdaus67@gmail.com>
2026-09-25 09:36:45 +0800

    .

--- random/test.js
+++ random/test.js
@@ -5,9 +5,7 @@ MyApp.calculator = {
     add: function (a, b) { // ==> equivalent to function add(a,b) {...};
         return a + b;
     },
-    subtract: function (a, b) {
-        return a - b;
-    }
+    subtract: (a, b) => a - b, // ==> don't put ; after the return value for arrow functions.
 };
 
 console.log(MyApp.calculator.add);
